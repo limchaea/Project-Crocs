e10cd108db22a816af670c7a8a4a99e4ee8e4e0b
kwon hana <vvvhana@gmail.com>
2025-11-04 18:23:19 +0900

    test - Collabs 내용변경

--- src/pages/Collabs.jsx
+++ src/pages/Collabs.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Collabs = () => {
-    return <div>Collabs</div>;
+    return <div>
+        <p>테스트입니다.Collabs</p>
+        </div>;
 };
 
 export default Collabs;
