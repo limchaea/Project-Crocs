69aadf880e50d772d299b45ba15aa410d323628c
JEON WOO JUN <ezenproject1@gmail.com>
2025-11-12 17:36:50 +0900

    25-11-12신규등록(jsx4개,이미지2개,scss4개, js1개)

--- src/pages/Collabs.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Collabs = () => {
-    return <div>Collabs</div>;
-};
-
-export default Collabs;
--- src/pages/Jibbiz.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Jibbiz = () => {
-    return <div>Jibbiz</div>;
-};
-
-export default Jibbiz;
